d6508b029077d63ddc0e972e533607f890574f02
Gupi-nath <it23019@mbstu.ac.bd>
2024-10-24 10:13:22 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+class Testlab {
+    Testlab() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("It's a rainy day!");
+    }
+}
